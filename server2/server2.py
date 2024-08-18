213ebbaa1c26f17b597506703a365e6af6a8a7a5
GOAT17A <warkadangad1@gmail.com>
2024-08-18 20:41:45 +0530

    server2

--- server2/server2.py
+++ server2/server2.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 @app.route('/message', methods=['POST'])
 def handle_message():
-    
+   
     data = request.json
     
    
     message = data.get('message', '')
     
-  
+    
     print(f"Received message: {message}")
     
- 
+   
     response = {"response": "Hi"}
     
     return jsonify(response), 200
 
 if __name__ == '__main__':
- 
+   
     app.run(host='0.0.0.0', port=5000)
